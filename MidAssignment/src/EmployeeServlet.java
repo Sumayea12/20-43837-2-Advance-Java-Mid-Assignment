3689b75730fff5e50195fba36e1860fa8fe4d695
Sumayea12 <sumayea429@gmail.com>
2023-11-19 03:46:12 +0600

    Done

---
+++ MidAssignment/src/EmployeeServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/addEmployee")
+public class EmployeeServlet extends HttpServlet {
+    private List<Employee> employees = new ArrayList<>();
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        LocalDate dob = LocalDate.parse(request.getParameter("dob"));
+        String email = request.getParameter("email");
+        LocalDate joiningDate = LocalDate.parse(request.getParameter("joiningDate"));
+
+        Employee employee = new Employee(id, name, dob, email, joiningDate);
+        LeaveCalculator.calculateLeaves(employee);
+        employees.add(employee);
+
+    }
+
+
+}
